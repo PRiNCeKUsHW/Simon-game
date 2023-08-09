a61482e82417afa8d10c69bd2833d17346ce4425
Anuj kumar Kushwaha <87234329+PRiNCeKUsHW@users.noreply.github.com>
2023-08-09 21:51:13 +0530

    Update game.js

--- game.js
+++ game.js
@@ -2,18 +2,30 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
-var userClickedPattern=[];
+var userClickedPattern = [];
 
 var started = false;
 var level = 0;
 
-$(document).keypress(function(){
-    if(!started){
-        $("#level-title").text("Level "+level);
-        nextSequence();
-        started = true;
-    }
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
 });
+
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
+});
+
 function checkAnswer(currentLevel) {
 
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
@@ -34,43 +46,34 @@ function checkAnswer(currentLevel) {
       startOver();
     }
 }
-$(".btn").click(function() {
-
-    var userChosenColour = $(this).attr("id");
-    userClickedPattern.push(userChosenColour);
-  
-    playSound(userChosenColour);
-    animatePress(userChosenColour);
-  
-    checkAnswer(userClickedPattern.length-1);
-  });
 
 
-function nextSequence(){
-    var randomNumber = Math.floor(Math.random()*4);
-    var randomChosenColour = buttonColours[randomNumber];
-    gamePattern.push(randomChosenColour);
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
 
-    $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-    playSound(randomChosenColour);
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
 }
 
 function animatePress(currentColor) {
-    $("#" + currentColor).addClass("pressed");
-    setTimeout(function () {
-      $("#" + currentColor).removeClass("pressed");
-    }, 100);
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
 }
 
 function playSound(name) {
-    var audio = new Audio("sounds/" + name + ".mp3");
-    audio.play();
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 }
-
 
 function startOver() {
-    level = 0;
-    gamePattern = [];
-    started = false;
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
-
